9d4eb6ef6c853bb090c6b0c3ea8e1e6b85be5d3a
sajju234 <sajudarji17@gmail.com>
2026-03-08 17:55:04 +0530

    Added config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",   // ✅ ADD THIS LINE
+
   resolve: {
     dedupe: ['react', 'react-dom', 'react-router-dom', '@material-tailwind/react', 'framer-motion'],
   },
+
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom', '@material-tailwind/react', 'framer-motion'],
   }
